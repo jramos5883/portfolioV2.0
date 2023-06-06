ad768d100ca10fbd0f589edfc4db5fd7124ca711
John Ramos <jramos5883@gmail.com>
2023-06-06 13:51:39 -0700

    Finished building and styling intro section

--- portfoliov2.0-app/app/(components)/intro.comp.tsx/intro.comp.tsx
+++ portfoliov2.0-app/app/(components)/intro.comp.tsx/intro.comp.tsx
@@ -1,16 +1,22 @@
 export default function Intro() {
   return (
-    <div className="">
-      <p className="text-yellow">Hi, my name is</p>
-      <p className="text-white text-3xl">John Ramos.</p>
-      <p className="text-3xl">I build things for the web.</p>
-      <p className="">
+    <div className="m-20">
+      <p className="text-yellow text-3xl py-2">Hi, my name is</p>
+      <p className="text-white text-6xl font-bold py-2">John Ramos.</p>
+      <p className="text-grey text-6xl font-bold py-2">
+        I build things for the web.
+      </p>
+      <p className="text-grey max-w-lg text-2xl py-2">
         I am a Los Angeles based junior software engineer looking to break into
         the tech industry. Currently looking to take on new projects and
         challenges and continue to improve my coding skills and knowledge.
       </p>
-      <button className="p-2 mr-4 border-4 rounded-lg">Get In Touch</button>
-      <button className="p-2 border-4 rounded-lg">Resume</button>
+      <button className="text-yellow border-yellow-400 p-2 mr-4 border-4 rounded-lg py-2">
+        Get In Touch
+      </button>
+      <button className="text-yellow border-yellow-400 p-2 border-4 rounded-lg py-2">
+        Resume
+      </button>
     </div>
   );
 }
--- portfoliov2.0-app/app/(components)/navbar/navbar.comp.tsx
+++ portfoliov2.0-app/app/(components)/navbar/navbar.comp.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <div className="flex flex-row justify-between h-16 items-center">
-      <h1 className="px-4 text-3xl">Navbar</h1>
+      <h1 className="text-yellow px-4 text-3xl">Navbar</h1>
       <div className="flex flex-row">
         <p className="px-4 text-yellow">
           01. <span className="text-white">About Me</span>
