2af4f13a67e5dacdf8e3d100953b9315b2799837
John Ramos <jramos5883@gmail.com>
2023-06-09 14:06:42 -0700

    Working on design for project section of portfolio.

---
+++ portfoliov2.0-app/app/(components)/projects/projects.comp.tsx
@@ -0,0 +1,7 @@
+export default function Projects() {
+  return (
+    <div className="border border-8 py-96 border-yellow-400 rounded-lg">
+      <h1 className="text-center text-5xl text-yellow">Project Section!</h1>
+    </div>
+  );
+}
--- portfoliov2.0-app/app/page.tsx
+++ portfoliov2.0-app/app/page.tsx
@@ -1,11 +1,13 @@
 import Intro from "./(components)/intro/intro.comp";
 import AboutMe from "./(components)/aboutme/aboutme.comp";
+import Projects from "./(components)/projects/projects.comp";
 
 export default function App() {
   return (
     <div>
       <Intro />
       <AboutMe />
+      <Projects />
     </div>
   );
 }
