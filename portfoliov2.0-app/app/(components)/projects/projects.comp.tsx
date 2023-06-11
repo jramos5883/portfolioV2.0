e02427aaefc85bf84457756f17bcd90988369f21
John Ramos <jramos5883@gmail.com>
2023-06-11 09:13:20 -0700

    Working on design for project section.

--- portfoliov2.0-app/app/(components)/projects/projects.comp.tsx
+++ portfoliov2.0-app/app/(components)/projects/projects.comp.tsx
@@ -1,5 +1,6 @@
 export default function Projects() {
   return (
+    // need to work on design and layout for project section
     <div className="border border-8 py-96 border-yellow-400 rounded-lg">
       <h1 className="text-center text-5xl text-yellow">Project Section!</h1>
     </div>
