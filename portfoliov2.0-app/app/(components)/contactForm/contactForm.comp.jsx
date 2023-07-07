c8d9cee1020eb2467e69e1dc91a5698aa5159bfb
John Ramos <jramos5883@gmail.com>
2023-07-06 20:53:32 -0700

    Finished building and styling contact form, need to finish project section and then deploy with cutsom domain.

--- portfoliov2.0-app/app/(components)/contactForm/contactForm.comp.jsx
+++ portfoliov2.0-app/app/(components)/contactForm/contactForm.comp.jsx
@@ -1,14 +1,28 @@
+import { montFont } from "@/app/fonts";
+
 export default function ContactForm() {
   return (
     <div className="m-20">
       <p className="text-yellow text-3xl">
         03. <span className="text-white text-3xl">Contact Form</span>
       </p>
-      <div className="flex justify-center items-center">
+      <div className="flex  flex-col items-center justify-center py-20">
+        <p
+          className={`${montFont.className} text-white text-6xl font-bold py-2`}
+        >
+          Get In Touch!
+        </p>
+        <p className="text-grey text-2xl py-4 indent max-w-2xl">
+          I am currently looking to make my break into tech and find my place in
+          the industry. I am also working on expanding my network and meeting
+          more like minded people. If you have any questions or would just like
+          to say hi, feel free to send me a message!
+        </p>
         <form
           target="_blank"
           action="https://formsubmit.co/45e1dbe943177426372966c2f8438520"
           method="POST"
+          className="w-full max-w-2xl" // Set maximum width to limit the form width
         >
           {/* Honeypot */}
           <input type="text" name="_honey" style={{ display: "none" }} />
@@ -16,47 +30,52 @@ export default function ContactForm() {
           {/* Disable Captcha */}
           <input type="hidden" name="_captcha" value="false" />
 
-          <div className="form-group">
-            <div className="form-row">
-              <div className="col">
-                <input
-                  type="text"
-                  name="Name&nbsp;/&nbsp;Alias"
-                  className="form-control"
-                  placeholder="Full Name / Buisness Name"
-                  required
-                />
-              </div>
-              <div className="col">
-                <input
-                  type="email"
-                  name="Email"
-                  className="form-control"
-                  placeholder="Email Address"
-                  required
-                />
-              </div>
-              <div className="col">
-                <input
-                  type="tel"
-                  name="Phone&nbsp;Number"
-                  className="form-control"
-                  placeholder="Phone Number (optional)"
-                />
-              </div>
+          <div className="flex flex-col">
+            <div className="mb-4">
+              <label className="text-yellow text-2xl">Name:</label>
+              <input
+                type="text"
+                name="Name&nbsp;/&nbsp;Alias"
+                className="w-full border rounded py-2 px-3 text-grey-darker"
+                placeholder="Full Name / Buisness Name"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="text-yellow text-2xl">Email:</label>
+              <input
+                type="email"
+                name="Email"
+                className="w-full border rounded py-2 px-3 text-grey-darker"
+                placeholder="Email Address"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="text-yellow text-2xl">Phone Number:</label>
+              <input
+                type="tel"
+                name="Phone&nbsp;Number"
+                className="w-full border rounded py-2 px-3 text-grey-darker"
+                placeholder="Phone Number (optional)"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="text-yellow text-2xl">Message:</label>
+              <textarea
+                placeholder="Your Message"
+                className="w-full border rounded py-2 px-3 text-grey-darker"
+                name="Message"
+                rows="10"
+                required
+              ></textarea>
             </div>
           </div>
-          <div className="form-group">
-            <textarea
-              placeholder="Your Message"
-              className="form-control"
-              name="Message"
-              rows="10"
-              required
-            ></textarea>
-          </div>
-          <button type="submit" className="">
-            Submit Form
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
+          >
+            Send Message
           </button>
         </form>
       </div>
