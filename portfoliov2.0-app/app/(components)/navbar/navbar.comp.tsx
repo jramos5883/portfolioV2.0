a081cd0af3442c367323c9105da17702996f1cd0
jramos5883 <jramos5883@gmail.com>
2023-07-27 20:07:59 -0700

    nav fix, portfolio on main pc now

--- portfoliov2.0-app/app/(components)/navbar/navbar.comp.tsx
+++ portfoliov2.0-app/app/(components)/navbar/navbar.comp.tsx
@@ -2,24 +2,24 @@ import { GiFlamingTrident } from "react-icons/gi";
 
 export default function Navbar() {
   return (
-    <div className="flex flex-row justify-between h-16 items-center">
-      <span className="text-yellow px-4 text-5xl">
+    <div className="flex flex-row items-center justify-between h-16">
+      <span className="px-4 text-5xl text-yellow">
         <GiFlamingTrident />
       </span>
       <div className="flex flex-row">
         <a href="#id_aboutMe">
-          <p className="px-4 text-yellow text-3xl">
-            01. <span className="text-white text-3xl">About Me</span>
+          <p className="px-4 text-3xl text-yellow">
+            01. <span className="text-3xl text-white">About Me</span>
           </p>
         </a>
         <a href="#id_projects">
-          <p className="px-4 text-yellow text-3xl">
-            02. <span className="text-white text-3xl">Projects</span>
+          <p className="px-4 text-3xl text-yellow">
+            02. <span className="text-3xl text-white">Projects</span>
           </p>
         </a>
         <a href="#id_contact">
-          <p className="px-4 text-yellow text-3xl">
-            03. <span className="text-white text-3xl">Contact Form</span>
+          <p className="px-4 text-3xl text-yellow">
+            03. <span className="text-3xl text-white">Contact Form</span>
           </p>
         </a>
       </div>
