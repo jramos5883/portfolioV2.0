59935b0ff1277c7a67abe6e2f5120c46be7913a3
John Ramos <jramos5883@gmail.com>
2023-07-30 13:05:13 -0700

    May need to touch up portfolio.

--- portfoliov2.0-app/app/(components)/footer/footer.comp.tsx
+++ portfoliov2.0-app/app/(components)/footer/footer.comp.tsx
@@ -1,7 +1,8 @@
 export default function Footer() {
+  // May need to add more too footer
   return (
     <div className="flex justify-center items-center text-3xl text-yellow p-4">
-      Copyright 2023
+      Copyright @ 2023
     </div>
   );
 }
